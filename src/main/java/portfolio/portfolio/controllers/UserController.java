e5559d88d60bcb0cd76bb8037565d87ee8e5cf40
AbrilSabatini <abrilsabatini2004@gmail.com>
2025-03-04 12:12:18 -0300

    Add getUsersNames method

--- src/main/java/portfolio/portfolio/controllers/UserController.java
+++ src/main/java/portfolio/portfolio/controllers/UserController.java
@@ -1,10 +1,15 @@
 package portfolio.portfolio.controllers;
 
+import org.springframework.http.ResponseEntity;
 import portfolio.portfolio.dtos.UserDtos.UserCreateDto;
 import portfolio.portfolio.dtos.UserDtos.UserResponseDto;
+import portfolio.portfolio.dtos.UserDtos.UserShortDto;
 import portfolio.portfolio.dtos.UserDtos.UserUpdateDto;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserController extends BaseController<UUID, UserCreateDto, UserUpdateDto, UserResponseDto> {
+
+    ResponseEntity<List<UserShortDto>> getUsersName();
 }
